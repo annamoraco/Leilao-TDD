10289f42112fb984a41b2a008d490523a28e777f
Anna Moraco <annamoraco@gmail.com>
2021-05-26 17:35:57 -0300

    Teste automatizado - estrutura básica

--- teste-avaliador.cpp
+++ teste-avaliador.cpp
@@ -5,17 +5,31 @@
 #include "Usuario.hpp"
 
 int main() {
+
+	// Preparação do ambiente - organiza as condições/cenário para o teste
+
 	Lance primeiroLance(Usuario("Anna Moraco"), 1000);
 	Lance segundoLance(Usuario("Rodrigo Mendes"), 1500);
 	Leilao leilao("Fiat 147 0km");
 	leilao.recebeLance(segundoLance);
 	leilao.recebeLance(primeiroLance);
 
+	// Execução do código que será testado
 
 	Avaliador leiloeiro;
 	leiloeiro.avalia(leilao);
 
-	std::cout << leiloeiro.recuperaMaiorValor() << std::endl;
+	//  Verificação se o teste teve sucesso ou falha
+
+	float valoresperado = 1500;
+
+	if (valoresperado == leiloeiro.recuperaMaiorValor()) {
+		std::cout << "TESTE OK" << std::endl;
+	}
+	else {
+		std::cout << "TESTE FALHOU" << std::endl;
+	}
+	
 
 	return 0;
 }
